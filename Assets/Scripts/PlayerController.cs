723307f2c2002e2b9d4f36dbabe3c0cd106b165c
lucyach <achesonlucy@gmail.com>
2026-04-06 20:58:38 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+    public float speed;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+    void OnMove (InputValue movementValue) {
+       Vector2 movementVector = movementValue.Get<Vector2>();
+       movementX = movementVector.x;
+       movementY = movementVector.y;
+   }
+
+   void FixedUpdate() {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+   }
+
+   void OnTriggerEnter (Collider other) 
+   {
+       if (other.gameObject.CompareTag("PickUp")) 
+       {
+           other.gameObject.SetActive(false);
+           count++;
+           SetCountText();
+       }
+   } 
+
+   void SetCountText() {
+       countText.text = "Count: " + count.ToString();
+       if (count >= 6) 
+       {
+           winTextObject.SetActive(true);
+       }
+   }
+}
